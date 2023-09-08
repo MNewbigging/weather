2f040378d2fc6eca4695f32657fec5f9e8ae0100
MNewbigging <mrnewbigging@gmail.com>
2023-09-08 14:29:23 +0100

    scene settings

--- src/app/scene/weather-scene.ts
+++ src/app/scene/weather-scene.ts
@@ -8,20 +8,23 @@ export class WeatherScene {
   private scene = new THREE.Scene();
   private camera: THREE.PerspectiveCamera;
   private renderer: Renderer;
-  private orbitControls: OrbitControls;
+  private orbitControls?: OrbitControls;
 
   constructor(canvas: HTMLCanvasElement, private loader: ModelLoader) {
     this.scene.background = new THREE.Color('#1680AF');
 
-    this.camera = new THREE.PerspectiveCamera();
+    this.camera = new THREE.PerspectiveCamera(
+      45,
+      canvas.clientWidth / canvas.clientHeight,
+      1,
+      100
+    );
+    this.camera.position.set(0, 2, 10);
 
     this.renderer = new Renderer(canvas, this.camera, this.scene);
 
-    this.orbitControls = new OrbitControls(
-      this.camera,
-      this.renderer.renderer.domElement
-    );
-    this.orbitControls.enableDamping = true;
+    // this.orbitControls = new OrbitControls(this.camera, canvas);
+    // this.orbitControls.enableDamping = true;
 
     this.setupScene();
   }
@@ -31,14 +34,23 @@ export class WeatherScene {
   }
 
   private setupScene() {
+    // Objects
     const objects = this.loader.get('scene');
     this.scene.add(objects);
+
+    // Lighting
+    const ambientLight = new THREE.AmbientLight();
+    this.scene.add(ambientLight);
+
+    const directLight = new THREE.DirectionalLight();
+    directLight.position.set(0, 1, 1);
+    this.scene.add(directLight);
   }
 
   private update = () => {
     requestAnimationFrame(this.update);
 
-    this.orbitControls.update();
+    //this.orbitControls.update();
 
     this.renderer.render();
   };
